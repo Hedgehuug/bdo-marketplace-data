b7d096135dc3c6e1a636d9c5d62ed83884ac97d8
Hedgehuug <47126622+Hedgehuug@users.noreply.github.com>
2021-04-27 18:25:58 +0200

    Push 1

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 import requests as r
 import data.url_list as urls
+import json
 
 url = urls.url_list['item_details']
 
@@ -15,19 +16,25 @@ payload = {
 
 
 response = r.request('POST',url,json = payload,headers = headers)
+dict_response = json.loads(response.content)['resultMsg']
 
 
 def item_info_cleanser(rsp,enhancement):
-    rsp = rsp.split(':')
-    rsp = rsp[2]
     rsp = rsp.split('|')
     rsp = rsp[enhancement]
     rsp = rsp.split('-')
     return rsp
 
-print(item_info_cleanser(response.text,3))
+# print(item_info_cleanser(response.text,3))
 
 class Api_Request:
     def __init__(self,headers={},payload = {}):
         self._headers = headers
         self._payload = payload
+
+
+    def get_payload(self):
+        return self._payload
+
+test_class = Api_Request(payload=payload)
+
